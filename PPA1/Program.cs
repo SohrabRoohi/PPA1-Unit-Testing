e71481385aaf18b9434dbe94d36420077adc7826
Sohrab <sohrabroohi@ufl.edu>
2019-09-09 23:02:41 -0400

    Added Split function and tests / Completed CLI

--- PPA1/Program.cs
+++ PPA1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PPA1
 {
@@ -27,30 +28,138 @@ namespace PPA1
                 }
                 catch(Exception e)
                 {
-                    goto skip;
+                    continue;
                 }
                 switch(num)
                 {
                     case 1:
-                        f.BMI(1,1,1);
+                        BMIInput();
                         break;
                     case 2:
+                        RetireInput();
                         break;
                     case 3:
+                        DistanceInput();
                         break;
                     case 4:
+                        SplitInput();
                         break;
                     case 5:
                         goto exit;
                     default:
-                        goto skip;
-
+                        continue;
                 }
-                skip:
-                continue;
             }
             exit:
             return; 
+        }
+
+        static void BMIInput()
+        {
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Enter 3 numbers on each line in the form: \nheightInFeet (double) \nHeightInInches (double) \nWeightInPounds (double)");
+                double heightInFeet, heightInInches, weightInPounds;
+                try
+                {
+                    heightInFeet = Double.Parse(Console.ReadLine());
+                    heightInInches = Double.Parse(Console.ReadLine());
+                    weightInPounds = Double.Parse(Console.ReadLine());
+                }
+                catch(Exception e)
+                {
+                    continue;
+                }
+                Console.WriteLine();
+                Console.WriteLine(f.BMI(heightInFeet, heightInInches, weightInPounds));
+                Console.WriteLine();
+                break;
+            }
+        }
+
+        static void RetireInput()
+        {
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Enter 4 numbers on each line in the form: \nage (int) \nsalary (double) \npercentage (double) \ngoal (double)");
+                int age;
+                double salary, percentage, goal;
+                try
+                {
+                    age = Int32.Parse(Console.ReadLine());
+                    salary = Double.Parse(Console.ReadLine());
+                    percentage = Double.Parse(Console.ReadLine());
+                    goal = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+                Console.WriteLine();
+                Console.WriteLine(f.Retire(age, salary, percentage, goal));
+                Console.WriteLine();
+                break;
+            }
+        }
+
+        static void DistanceInput()
+        {
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Enter 4 numbers on each line in the form: \nx1 (double) \ny1 (double) \nx2 (double) \ny2 (double)");
+                double x1, y1, x2, y2;
+                try
+                {
+                    x1 = Double.Parse(Console.ReadLine());
+                    y1 = Double.Parse(Console.ReadLine());
+                    x2 = Double.Parse(Console.ReadLine());
+                    y2 = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+                Console.WriteLine();
+                Console.WriteLine(f.Distance(x1, y1, x2, y2));
+                Console.WriteLine();
+                break;
+            }
+        }
+
+        static void SplitInput()
+        {
+            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine("Enter 2 numbers on each line in the form: \namount (double without dollar sign) \nnumberOfPeople (int)");
+                double amount;
+                int numberOfPeople;
+                try
+                {
+                    amount = Double.Parse(Console.ReadLine());
+                    numberOfPeople = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+                Console.WriteLine();
+                List<double> l = f.Split(amount, numberOfPeople);
+                for(int i = 0; i < l.Count; i++)
+                {
+                    if(i != 0)
+                    {
+                        Console.Write(", ");
+                    }
+                    Console.Write("$" + l[i]);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                break;
+            }
         }
     }
 }
